6f2c96bcc015447d8ee70cab0c7fda563ed07b46
akanieki <cgkn1219@gmail.com>
2026-02-22 20:08:31 +0300

    Add files via upload

--- app/src/main/java/com/biblinder/data/local/BiblinderDatabase.kt
+++ app/src/main/java/com/biblinder/data/local/BiblinderDatabase.kt
@@ -2,8 +2,18 @@ package com.biblinder.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
-@Database(entities = [AnimeEntity::class], version = 1, exportSchema = false)
+@Database(
+    entities = [
+        AnimeEntity::class,
+        AnimeListItem::class,
+        CustomList::class
+    ],
+    version = 2,
+    exportSchema = false
+)
+@TypeConverters(Converters::class)
 abstract class BiblinderDatabase : RoomDatabase() {
     abstract fun animeDao(): AnimeDao
 }
